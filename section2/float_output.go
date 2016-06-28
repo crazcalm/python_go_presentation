1f5bcf35177108f8d354edc3618f6f0a5f330c80
crazcalm <crazcalm@gmail.com>
2016-06-27 21:02:53 -0400

    float output is done

--- section2/float_output.go
+++ section2/float_output.go
@@ -5,14 +5,14 @@ import "math/rand"
 import "time"
 
 func init(){
-    rand.Seed(time.Now().UTC().UnixNano())
+    rand.Seed(time.Now().UnixNano())
 }
 
-//export int_output
-func int_output() int {
+//export float_output
+func float_output() float32 {
     // Look into seeding a random generator
     // figure out how to get random number.
-    return rand.Intn(10000)
+    return (rand.Float32() + float32(1)) * float32(rand.Intn(1000))
 }
 
 func main(){}
