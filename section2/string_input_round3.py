fd64129a9e0d8e22972bd4965eb82dc1f1d17ae9
crazcalm <crazcalm@gmail.com>
2016-07-14 22:26:04 -0400

    I can now pass strings from python to Gogit add .git add .! YES

--- section2/string_input_round3.py
+++ section2/string_input_round3.py
@@ -2,9 +2,9 @@ import ctypes
 import os
 
 def main():
-    path = os.path.join(os.path.abspath("."), "string_input.so")
+    path = os.path.join(os.path.abspath("."), "string_input_cgo.so")
     lib = ctypes.CDLL(path)
-    user_input = ctypes.c_wchar_p("Hello")
+    user_input = ctypes.c_char_p(b"Hello")
     print("Answer is:")
     print("-------------------")
     lib.string_input(user_input)
