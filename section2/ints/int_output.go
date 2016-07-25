7e08a4b4cb75ef0d9ff4f3e3ba09eba3f3dbd998
crazcalm <crazcalm@gmail.com>
2016-07-25 19:15:12 -0400

    int_output.go now accepts user input

--- section2/ints/int_output.go
+++ section2/ints/int_output.go
@@ -1,18 +1,34 @@
 package main
 
-import "C"
-import "math/rand"
-import "time"
-
-func init() {
-	rand.Seed(time.Now().UTC().UnixNano())
-}
+import (
+	"C"
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 //export int_output
 func int_output() int {
-	// Look into seeding a random generator
-	// figure out how to get random number.
-	return rand.Intn(10000)
+	fmt.Println("Please enter an integer:")
+	input := bufio.NewScanner(os.Stdin)
+	var result int
+
+	for input.Scan() {
+		text := strings.TrimSpace(input.Text())
+		num, err := strconv.Atoi(text)
+		if err != nil {
+			fmt.Printf("%s is not a valid integer\nTry again: ", text)
+		} else {
+			result = num
+			break
+		}
+	}
+
+	return result
 }
 
-func main() {}
+func main() {
+	int_output()
+}
